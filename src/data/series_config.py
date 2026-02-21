2d19a731b972e24dc1eeb56e5e513e275a104278
Miles' Mac Mini <miles-mac-mini@Miless-Mac-mini.local>
2026-02-20 23:41:59 -0500

    Add 8 more FRED series to series_config

--- src/data/series_config.py
+++ src/data/series_config.py
@@ -8,5 +8,45 @@ SERIES = {
         'id': 'UNRATE',
         'name': 'Unemployment Rate',
         'frequency': 'monthly'
+    },
+    'real_gdp': {
+        'id': 'GDPC1',
+        'name': 'Real GDP',
+        'frequency': 'quarterly'
+    },
+    '10y-2y_treasury': {
+        'id': 'T10Y2Y',
+        'name': '10 Year - 2 Year Treasury Spread',
+        'frequency': 'daily'
+    },
+    'fed_funds_rate': {
+        'id': 'FEDFUNDS',
+        'name': 'Federal Funds Effective Rate',
+        'frequency': 'monthly'
+    },
+    'housing_starts': {
+        'id': 'HOUST',
+        'name': 'Housing Starts',
+        'frequency': 'monthly'
+    },
+    'consumer_sentiment': {
+        'id': 'UMCSENT',
+        'name': 'Consumer Sentiment',
+        'frequency': 'monthly'
+    },
+    'nonfarm_payrolls': {
+        'id': 'PAYEMS',
+        'name': 'Nonfarm Payrolls',
+        'frequency': 'monthly'
+    },
+    'pce_price_index': {
+        'id': 'PCEPI',
+        'name': 'PCE Price Index',
+        'frequency': 'monthly'
+    },
+    'crude_oil_wti': {
+        'id': 'DCOILWTICO',
+        'name': 'Crude Oil(WTI)',
+        'frequency': 'daily'
     }
 }
